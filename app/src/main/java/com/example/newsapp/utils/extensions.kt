bc66fb99aebebf0f95f4e8a122ac8c38043163d4
junaid <junaid@cognitivehealthintl.com>
2022-03-29 23:05:48 +0500

    cleanup...

--- app/src/main/java/com/example/newsapp/utils/extensions.kt
+++ app/src/main/java/com/example/newsapp/utils/extensions.kt
@@ -1,11 +1,6 @@
 package com.example.newsapp.utils
 
-
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-import java.lang.Exception
-import java.lang.reflect.Type
-
 
 fun Any.serialize(): String = Gson().toJson(this)
 
