2d367fc1cfb4b9ab4743743c557a00d51d6b32ae
junaid <junaid@cognitivehealthintl.com>
2022-03-29 15:09:00 +0500

    changed date format...

--- app/src/main/java/com/example/newsapp/network/responses.kt
+++ app/src/main/java/com/example/newsapp/network/responses.kt
@@ -1,5 +1,9 @@
 package com.example.newsapp.network
 
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+
 data class TopHeadlinesResponse(
     var status: String? = null,
     val totalResults: Int? = null,
@@ -16,6 +20,17 @@ data class Article(
     var publishedAt: String? = null,
     var content: String? = null
 )
+{
+    val publishedTime: String
+        get() {
+            return if (publishedAt != null) {
+                val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
+                simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+                val timestamp = Timestamp(simpleDateFormat.parse(publishedAt!!)!!.time)
+                timestamp.toString()
+            } else ""
+        }
+}
 
 data class Source(
     var id: String? = null,
